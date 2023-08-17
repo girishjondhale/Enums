51047c8bafbfb9d86d430e74392cd80a90fe8d7b
girishjondhale <girishjondhale1997@gmail.com>
2023-08-16 17:52:31 -0700

    Add project files.

---
+++ Enums/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Builder
+{
+
+    public enum Streams { CS, IT, Electrical, Electronic, Mech, Civil };
+    public class Student
+    {
+        private int rollno, m1, m2, m3;
+        private string name;
+        double percentage, total;
+        private Streams streams;
+        public Student(string name, int rollno, int m1, int m2, int m3, Streams streams)
+        {
+            this.name = name;
+            this.rollno = rollno;
+            this.streams = streams;
+            this.m1 = m1;
+            this.m2 = m2;
+            this.m3 = m3;
+        }
+        public void Calculate()
+        {
+            total = m1 + m2 + m3;
+            percentage = (total / 300) * 100;
+        }
+
+        public override string ToString()
+        {
+            return $"The name is = {name}\n The roll no is = {rollno}\n The percentage is = {percentage}.\n stream is = {streams}";
+        }
+    }
+
+
+}
---
+++ Enums/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static String_Builder.Employee;
+
+namespace String_Builder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Employee1 emp1 = new Employee1("aaa", 20000, Dept.Sales);
+                emp1.CalculateSalary();
+                Console.WriteLine(emp1.PrintEmpSalary());
+                Console.WriteLine(emp1);
+
+                Employee1 emp2 = new Employee1("Ab", 39000, Dept.Testing);
+                emp2.CalculateSalary();
+                Console.WriteLine(emp2.PrintEmpSalary());
+                Console.WriteLine(emp2);
+
+                Student s1 = new Student("zzz", 12345, 80, 92, 95, Streams.CS);
+                s1.Calculate();
+                Console.WriteLine(s1.ToString());
+
+            }
+
+        }
+    }
+}
